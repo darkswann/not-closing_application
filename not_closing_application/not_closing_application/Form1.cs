4dedbc16982d751122aca092138c03bf06038663
Dârk Swân <80572575+darkswann@users.noreply.github.com>
2022-01-08 11:19:45 +0300

    Add files via upload

---
+++ not_closing_application/not_closing_application/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace not_closing_application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DialogResult warn = new DialogResult();
+            warn = MessageBox.Show("Kapanmıyom Tıklamasaydın", "Salak", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+
+            if (warn == DialogResult.OK)
+            {
+                MessageBox.Show("Kapanmıyom", "Salak", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Form1 darkform = new Form1();
+                darkform.Show();
+            }
+
+            else
+            {
+                MessageBox.Show("Yıkılmam Kapanmam", "Salak", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Form1 darkform = new Form1();
+                darkform.Show();
+            }
+        }
+    }
+}
